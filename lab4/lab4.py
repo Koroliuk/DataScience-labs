e8b5c0430dc4cc08fb2561b4d54c5f7af2ecc212
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-05-09 23:45:13 +0300

    Refactor lab4.py

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,37 +1,51 @@
 import numpy as np
 import pandas as pd
 
+# ------------------------------------- сегмент API ------------------------------------
+# Шлях до файлу з вхідними даними
 input_data_file_path = "/home/koroliuk/PycharmProjects/DataScience-labs/lab4/data.xlsx"
-elements = ["Сайт1", "Сайт2", "Сайт3", "Сайт4", "Сайт5", "Сайт6", "Сайт7", "Сайт8", "Сайт9", "Сайт10", "Сайт11",
-            "Сайт12"]
-criteria_column = "Критерій"
-G = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-G_group = [1, 1, 1, 1]
-d = 0.1
+elements = ["Сайт1", "Сайт2", "Сайт3", "Сайт4",
+            "Сайт5", "Сайт6", "Сайт7", "Сайт8",             # Масив, що містить назви колонок елементів дослідження
+            "Сайт9", "Сайт10", "Сайт11", "Сайт12"]
+criteria_column = "Критерій"                                # Назва колонки, яка визначає природу критерію фактора
 
-factors_count = len(G)
-elements_count = len(elements)
-G_normalized = np.zeros(factors_count)
-G_group_normalized = np.zeros(len(G_group))
-G_sum = sum(G)
-G_group_sum = sum(G_group)
-for i in range(factors_count):
-    G_normalized[i] = G[i] / G_sum
+G = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]        # Масив, що містить вагові коефіцієнти факторів
+G_group = [1, 1, 1, 1]                                      # Масив, що містить вагові коефіцієнти груп факторів
 
-G_group_sum = sum(G_group)
-for i in range(len(G_group)):
-    G_group_normalized[i] = G_group[i] / G_group_sum
+d = 0.1                                                     # Коефіцієнт запасу
 
-factors = np.zeros((factors_count, elements_count), float)
+factors_count = len(G)                                      # Кількість факторів
+group_count = len(G_group)                                  # Кількість елементів
+elements_count = len(elements)                              # Кількість груп факторів
+
+
+# Функція, що приймає на вхід масив та нормалізує його
+def normalize_array(arr):
+    arr_length = len(arr)
+    arr_sum = sum(arr)
+    res = [arr[k]/arr_sum for k in range(arr_length)]
+    return np.array(res)
+
+
+# ------------------------------------- Зчитування даних з файлу ------------------------------------
+factors = np.zeros((factors_count, elements_count), float)          # Масив зі значеннями факторів
 input_data = pd.read_excel(input_data_file_path, engine='openpyxl')
 for i in range(factors_count):
     for j in range(elements_count):
-        factors[i, j] = input_data[elements[j]][i]
+        factors[i, j] = input_data[elements[j]][i]      # Зчитуємо дані з файлу в масив
 
-criteria = {}
+criteria = {}                                           # Словник, що визначає критерій кожного фактору
 for i in range(factors_count):
     criteria[i] = input_data[criteria_column][i]
 
+# ------------------------ Власне алгоритм вирішення багатокритеріальної задачі -----------------------
+# -------------------------- за критерієм згортки (використовується -----------------------------------
+# -------------------------- нелінійа схема компромісів А.М. Вороніна) --------------------------------
+
+G_normalized = normalize_array(G)                       # Нормалізовані вагові коефіцієнти факторів
+G_group_normalized = normalize_array(G_group)           # Нормалізовані вагові коефіцієнти груп факторів
+
+# Нормуємо значення критеріїв, що входять до згортки
 factors_normalized = np.zeros((factors_count, elements_count), float)
 for i in range(factors_count):
     factor = factors[i]
@@ -44,24 +58,26 @@ for i in range(factors_count):
         for j in range(elements_count):
             factors_normalized[i, j] = (min_value - d) / factor[j]
 
-factors_by_group = np.zeros((4, elements_count), float)
+# Проводимо зведення часткових факторів до узагальнених за групою
+factors_by_group = np.zeros((group_count, elements_count), float)
 for i in range(elements_count):
-    # 1st group
+    # Формуємо значення першої групи (рейтингові)
     for j in range(0, 3):
         factors_by_group[0, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
-    # 2nd group
+    # Формуємо значення другої групи (робота з сайтом)
     for j in range(3, 10):
         factors_by_group[1, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
-    # 3rd group
+    # Формуємо значення третьої групи (технічні)
     for j in range(10, 12):
         factors_by_group[2, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
-    # 4th group
+    # Формуємо значення четвертої групи (економічні)
     for j in range(12, 16):
         factors_by_group[3, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
+# Нормуємо частинні показники факторів для узагальненої оцінки
 factors_normalized_maximum = np.zeros((factors_count, elements_count), float)
 for i in range(factors_count):
     factor = factors[i]
@@ -74,40 +90,55 @@ for i in range(factors_count):
         for j in range(elements_count):
             factors_normalized_maximum[i, j] = (factor[j] - d) / factor[j]
 
-factors_by_group_maximum = np.zeros((4, elements_count), float)
+# Формуємо з отриманих значень масив, що містить вже максимальні значення для груп факторів
+factors_by_group_maximum = np.zeros((group_count, elements_count), float)       # Масив, що містить нормовані
 for i in range(elements_count):
-    # 1st group
+    # Формуємо значення першої групи
     for j in range(0, 3):
         factors_by_group_maximum[0, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
-    # 2nd group
+    # Формуємо значення другої групи
     for j in range(3, 10):
         factors_by_group_maximum[1, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
-    # 3rd group
+    # Формуємо значення третьої групи
     for j in range(10, 12):
         factors_by_group_maximum[2, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
-    # 4th group
+    # Формуємо значення четвертої групи
     for j in range(12, 16):
         factors_by_group_maximum[3, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
-factors_by_group_normalized = np.zeros((4, elements_count), float)
-for i in range(4):
+# Розраховуємо узагальнену нормовану оцінку для груп факторів
+factors_by_group_normalized = np.zeros((group_count, elements_count), float)
+for i in range(group_count):
     for j in range(elements_count):
         factors_by_group_normalized[i, j] = factors_by_group[i, j] / factors_by_group_maximum[i, j]
 
-integro = np.zeros(elements_count, float)
+# Отримуємо масив зі значеннями інтегрованої оцінки ефективності досліджуваних елементів
+result = np.zeros(elements_count, float)
 for i in range(elements_count):
-    for j in range(4):
-        integro[i] += G_group_normalized[j] * (1 - factors_by_group_normalized[j, i]) ** (-1)
+    for j in range(group_count):
+        result[i] += G_group_normalized[j] * (1 - factors_by_group_normalized[j, i]) ** (-1)
 
-max_values = []
-for i in range(4):
-    max_values.append((1 - max(factors_by_group_normalized[i])) ** (-1))
-max_integro = sum(max_values)
-integro_normalized = np.zeros(elements_count, float)
-for i in range(elements_count):
-    integro_normalized[i] = 1 - integro[i] / max_integro
+# ------------------------------ Інтерпретація результату ------------------------------------
+# Приведення значення до єдиної шкали (від 0 до 1)
+maximum_group_values = []    # Найгірші значення з можливих для груп факторів
+for i in range(group_count):
+    maximum_group_values.append((1 - max(factors_by_group_normalized[i])) ** (-1))
+max_res = sum(maximum_group_values)
 
-print(integro_normalized)
+result_in_single_scale = np.zeros(elements_count, float)    # Масив, що містить рішення багатокритеріальної
+for i in range(elements_count):                             # задачі в єдииній шкалі
+    result_in_single_scale[i] = 1 - result[i] / max_res
+
+# Пошук оптимального варіанту
+opt, opt_idx = 0, 0
+for i in range(group_count):
+    if opt < result_in_single_scale[i]:
+        opt = result_in_single_scale[i]
+        opt_idx = i
+
+print("Reslut = ", result_in_single_scale)
+print("Номер оптимального варіанту:", opt_idx+1)
+print("Власне оптимальний варіант:", elements[opt_idx])
