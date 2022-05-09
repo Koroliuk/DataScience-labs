0a93575a741eecd8985a8fa335cf4bcc2e35da45
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-05-09 22:21:02 +0300

    Add gama coefficients in calculus formulas

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -6,16 +6,23 @@ elements = ["Сайт1", "Сайт2", "Сайт3", "Сайт4", "Сайт5", "С
             "Сайт12"]
 criteria_column = "Критерій"
 G = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+G_group = [1, 1, 1, 1]
 d = 0.1
 
 factors_count = len(G)
 elements_count = len(elements)
 G_normalized = np.zeros(factors_count)
+G_group_normalized = np.zeros(len(G_group))
 G_sum = sum(G)
+G_group_sum = sum(G_group)
 for i in range(factors_count):
     G_normalized[i] = G[i] / G_sum
 
-factors = np.zeros((factors_count, elements_count),  float)
+G_group_sum = sum(G_group)
+for i in range(len(G_group)):
+    G_group_normalized[i] = G_group[i] / G_group_sum
+
+factors = np.zeros((factors_count, elements_count), float)
 input_data = pd.read_excel(input_data_file_path, engine='openpyxl')
 for i in range(factors_count):
     for j in range(elements_count):
@@ -25,37 +32,37 @@ criteria = {}
 for i in range(factors_count):
     criteria[i] = input_data[criteria_column][i]
 
-factors_normalized = np.zeros((factors_count, elements_count),  float)
+factors_normalized = np.zeros((factors_count, elements_count), float)
 for i in range(factors_count):
     factor = factors[i]
     if criteria[i] == 'min':
         max_value = max(factor)
         for j in range(elements_count):
-            factors_normalized[i, j] = factor[j]/(max_value+d)
+            factors_normalized[i, j] = factor[j] / (max_value + d)
     else:
         min_value = min(factor)
         for j in range(elements_count):
-            factors_normalized[i, j] = (min_value-d)/factor[j]
+            factors_normalized[i, j] = (min_value - d) / factor[j]
 
 factors_by_group = np.zeros((4, elements_count), float)
 for i in range(elements_count):
     # 1st group
     for j in range(0, 3):
-        factors_by_group[0, i] += (1 - factors_normalized[j, i])**(-1)
+        factors_by_group[0, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
     # 2nd group
     for j in range(3, 10):
-        factors_by_group[1, i] += (1 - factors_normalized[j, i]) ** (-1)
+        factors_by_group[1, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
     # 3rd group
     for j in range(10, 12):
-        factors_by_group[2, i] += (1 - factors_normalized[j, i]) ** (-1)
+        factors_by_group[2, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
     # 4th group
     for j in range(12, 16):
-        factors_by_group[3, i] += (1 - factors_normalized[j, i]) ** (-1)
+        factors_by_group[3, i] += G_normalized[j] * (1 - factors_normalized[j, i]) ** (-1)
 
-factors_normalized_maximum = np.zeros((factors_count, elements_count),  float)
+factors_normalized_maximum = np.zeros((factors_count, elements_count), float)
 for i in range(factors_count):
     factor = factors[i]
     if criteria[i] == 'min':
@@ -71,36 +78,36 @@ factors_by_group_maximum = np.zeros((4, elements_count), float)
 for i in range(elements_count):
     # 1st group
     for j in range(0, 3):
-        factors_by_group_maximum[0, i] += (1 - factors_normalized_maximum[j, i])**(-1)
+        factors_by_group_maximum[0, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
     # 2nd group
     for j in range(3, 10):
-        factors_by_group_maximum[1, i] += (1 - factors_normalized_maximum[j, i]) ** (-1)
+        factors_by_group_maximum[1, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
     # 3rd group
     for j in range(10, 12):
-        factors_by_group_maximum[2, i] += (1 - factors_normalized_maximum[j, i])**(-1)
+        factors_by_group_maximum[2, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
     # 4th group
     for j in range(12, 16):
-        factors_by_group_maximum[3, i] += (1 - factors_normalized_maximum[j, i])**(-1)
+        factors_by_group_maximum[3, i] += G_normalized[j] * (1 - factors_normalized_maximum[j, i]) ** (-1)
 
 factors_by_group_normalized = np.zeros((4, elements_count), float)
 for i in range(4):
     for j in range(elements_count):
-        factors_by_group_normalized[i, j] = factors_by_group[i, j]/factors_by_group_maximum[i, j]
+        factors_by_group_normalized[i, j] = factors_by_group[i, j] / factors_by_group_maximum[i, j]
 
 integro = np.zeros(elements_count, float)
 for i in range(elements_count):
     for j in range(4):
-        integro[i] += (1-factors_by_group_normalized[j, i])**(-1)
+        integro[i] += G_group_normalized[j] * (1 - factors_by_group_normalized[j, i]) ** (-1)
 
 max_values = []
 for i in range(4):
-    max_values.append((1-max(factors_by_group_normalized[i]))**(-1))
+    max_values.append((1 - max(factors_by_group_normalized[i])) ** (-1))
 max_integro = sum(max_values)
 integro_normalized = np.zeros(elements_count, float)
 for i in range(elements_count):
-    integro_normalized[i] = 1 - integro[i]/max_integro
+    integro_normalized[i] = 1 - integro[i] / max_integro
 
 print(integro_normalized)
