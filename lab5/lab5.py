e11ccb607aa3d061211207b4e617ac4ae803dba6
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-05-31 04:26:39 +0300

    Update parsing

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,104 +1,91 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import requests
 from bs4 import BeautifulSoup as bs
-import numpy as np
-import matplotlib.pyplot as plt
 
 
-input_data = {
-    'Всього інфіковано загалом': [],
-    'Смертельні випадки загалом': [],
-    'Видужали загалом': [],
-    'Наразі хворіють': [],
-    'Всього інфіковано за день': [],
-    'Смертельні випадки за день': [],
-    'Видужали за день': [],
-}
-
-base_url = 'https://index.minfin.com.ua/ua/reference/coronavirus/ukraine/hmelnickaya'
-data = [[], [], [], [], [], [], []]
+def get_formatted_date(year, month):
+    return str(year) + '-' + str(month).rjust(2, '0')
 
 
-def create_full_url(url, year, month):
-    return url + '/' + str(year) + '-' + str(month).rjust(2, '0') + '/'
+def create_full_url(url, date):
+    return url + '/' + date + '/'
 
 
-def get_values_from_response(response):
+def segment_by_month(arr):
+    d = {}
+    for date, value in arr:
+        if date not in d:
+            d[date] = 0
+        d[date] += value
+    return list(d.values())
+
+
+def get_values_from_response(response, date):
     if response.ok:
         soup = bs(response.text, 'lxml')
         tables = [div.table for div in soup.find_all('div',  class_='compact-table')]
-        total_table = tables[0]
-        for total_info in total_table.find_all('tr'):
-            if total_info.find('th'):
+        table = tables[-1]
+        day_infos = table.find_all('tr')
+        for day_info in day_infos:
+            if day_info.find('th'):
                 continue
-            total_info_arr = total_info.find_all('td', align='right')
-            for i in range(4):
-                found = total_info_arr[i].contents[0]
-                if found.name != 'br':
-                    data[i].append(int(found))
-                else:
-                    data[i].append(0)
-
-        per_day_table = tables[1]
-        for per_day_info in per_day_table.find_all('tr'):
-            if per_day_info.find('th'):
-                continue
-            per_day_info = per_day_info.find_all('td', align='right')
+            day_info = day_info.find_all('td', align='right')
             for i in range(3):
-                found = per_day_info[i].contents[0]
+                found = day_info[i].contents[0]
+                value = 0
                 if found.name != 'br':
-                    data[i+4].append(int(found))
-                else:
-                    data[i+4].append(0)
+                    value = int(found)
+                data[i].append((date, value))
     else:
         print("Проблеми зі з'єднанням")
 
 
+base_url = 'https://index.minfin.com.ua/ua/reference/coronavirus/ukraine/hmelnickaya'
 start_year, start_month = 2020, 3
 end_year, end_month = 2022, 1
+
+data = [[], [], []]
 session = requests.Session()
 while start_year < end_year or (start_year == end_year and start_month <= end_month):
-    full_url = create_full_url(base_url, start_year, start_month)
+    formatted_date = get_formatted_date(start_year, start_month)
+    full_url = create_full_url(base_url, formatted_date)
     request_response = session.get(full_url)
-    get_values_from_response(request_response)
+    get_values_from_response(request_response, formatted_date)
     if start_month != 12:
         start_month += 1
     else:
         start_month = 1
         start_year += 1
 
-print(data)
+infected_per_day = np.array([value for date, value in data[0]])
+dead_per_day = np.array([value for date, value in data[1]])
+recovered_per_day = np.array([value for date, value in data[2]])
 
-total_infected = np.array(data[0])
-total_dead = np.array(data[1])
-total_recovered = np.array(data[2])
-currently_ill = np.array(data[3])
-infected_per_day = np.array(data[4])
-dead_per_day = np.array(data[5])
-recovered_per_day = np.array(data[6])
+infected_per_month = np.array(segment_by_month(data[0]))
+dead_per_month = np.array(segment_by_month(data[1]))
+recovered_per_month = np.array(segment_by_month(data[2]))
 
-plt.plot(total_infected, label='Всього інфіковано загалом', color="blue")
-plt.plot(total_dead, label='Смертельні випадки загалом', color="black")
-plt.plot(total_recovered, label='Видужали загалом', color="green")
-plt.plot(currently_ill, label='Наразі хворіють', color="red")
+plt.plot(infected_per_day, label="Всього інфіковано за день", color="blue")
 plt.legend()
 plt.show()
 
-plt.plot(infected_per_day, label='Всього інфіковано за день', color="blue")
-plt.plot(dead_per_day, label='Смертельні випадки за день', color="black")
-plt.plot(recovered_per_day, label='Видужали за день', color="green")
+plt.plot(dead_per_day, label="Смертельні випадки за день", color="black")
 plt.legend()
 plt.show()
 
-
-plt.plot(infected_per_day, label='Всього інфіковано за день', color="blue")
+plt.plot(recovered_per_day, label="Видужали за день", color="green")
 plt.legend()
 plt.show()
 
-plt.plot(dead_per_day, label='Смертельні випадки за день', color="black")
+plt.plot(infected_per_month, label="Всього інфіковано за місяць", color="blue")
 plt.legend()
 plt.show()
 
-plt.plot(recovered_per_day, label='Видужали за день', color="green")
+plt.plot(dead_per_month, label="Смертельні випадки за місяць", color="black")
 plt.legend()
 plt.show()
 
+plt.plot(recovered_per_month, label="Видужали за місяць", color="green")
+plt.legend()
+plt.show()
